1f54228b0dfbc7a3803f6379b35260a554683c6c
kapui_niki <kapui_niki@sch.bme.hu>
2022-04-26 23:38:41 +0200

    MTP protocol done

--- src/server.py
+++ src/server.py
@@ -1,15 +1,27 @@
 import socket
 
+from MTP import decrypt, encrypt
+
 
 def run_server(host, port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((host, port))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            print(f"Connected by {addr}")
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                conn.sendall(data)
+        end = True
+        while end:
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                print(f"Connected by {addr}")
+                while True:
+                    data = conn.recv(4096)
+
+                    data = decrypt(data, b'\x00\x10', "server/rcvstate.txt")
+                    if data == 0:
+                        end = False
+                        break
+
+                    print(f"Received {data!r}")
+                    
+                    data = encrypt(data, b'\x00\x10',"server/sndstate.txt")
+
+                    conn.sendall(data)
