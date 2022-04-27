15212223a5273f9a38e30d6c8e4d7ec98f7308d5
kapui_niki <kapui_niki@sch.bme.hu>
2022-04-27 19:32:16 +0200

    multiple clients

--- src/server.py
+++ src/server.py
@@ -1,27 +1,30 @@
 import socket
+import _thread
 
 from MTP import decrypt, encrypt
 
+def on_new_client(conn, addr):
+    print(f"Connected by {addr}")
+    while True:
+        data = conn.recv(4096)
+
+        data = decrypt(data, b'\x00\x10', "server/rcvstate.txt")
+        if data == 0:
+            break
+
+        print(f"Received {data!r}")
+        
+        data = encrypt(data, b'\x00\x10',"server/sndstate.txt")
+
+        conn.sendall(data)
+    print(f"Disconnected {addr}")
+    conn.close()
 
 def run_server(host, port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((host, port))
-        end = True
-        while end:
-            s.listen()
+        s.listen()
+        while True:
             conn, addr = s.accept()
-            with conn:
-                print(f"Connected by {addr}")
-                while True:
-                    data = conn.recv(4096)
+            _thread.start_new_thread(on_new_client,(conn,addr))
 
-                    data = decrypt(data, b'\x00\x10', "server/rcvstate.txt")
-                    if data == 0:
-                        end = False
-                        break
-
-                    print(f"Received {data!r}")
-                    
-                    data = encrypt(data, b'\x00\x10',"server/sndstate.txt")
-
-                    conn.sendall(data)
