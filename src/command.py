70b5eca3cfdf12181d832fae97a7da01f5f49aa8
kapui_niki <kapui_niki@sch.bme.hu>
2022-05-06 01:02:25 +0200

    pwd done chd in progress

--- src/command.py
+++ src/command.py
@@ -1,27 +1,50 @@
+import os
 from Crypto.Hash import SHA256
+from cv2 import cubeRoot
 from MTP import COMMAND_REQ, COMMAND_RES, decrypt, encrypt
 
+def pwd(current_dir):
+    if current_dir == None:
+        return "failure"
+    if not os.path.exists("server" + current_dir):
+        return "failure"
+    else:
+        return "success" + '\n' + current_dir
+
+def get_message(command, results):
+    if command == "pwd":
+        if results[0] == "success":
+            return results[1]
+        else:
+            return "Current working directory not found!"
 
 def command_req(command):
-    message = command + '\n'
-    message += "param_1"
+    message = command
+    
+    if command == "pwd":
+        pass
+    if command == "chd":
+
 
     return message
 
-def command_res(command, message):
+def command_res(command, params, message, user, current_dir):
     SHA = SHA256.new()
     SHA.update(message)
     request_hash = SHA.digest()
 
     message = command + '\n'
     message += request_hash.hex() + '\n'
-    message += "result_1"
+    
+    if command == "pwd":
+        message += pwd(current_dir)
 
-    return message
+    return message, current_dir
 
 
 def command_client(socket, number, user):
     command = input(user + ":~$ ")
+    #TODO paraméterrel mi legyen? szét kéne szedni 
     if command not in ["pwd", "lst", "chd", "mkd", "del", "upl", "dnl"]:
         if command == "exit":
             return -1, None
@@ -48,14 +71,14 @@ def command_client(socket, number, user):
     data = msg.decode('utf-8').split('\n')
 
     request_hash = bytes.fromhex(data[1])
-    param_1 = data[2]
-
     if request_hash != hash:
         return -1, None
+    
+    message = get_message(command, data[2:])
 
-    return 0, None
+    return 0, message
 
-def command_server(conn, number, user):
+def command_server(conn, number, user, current_dir):
     data = conn.recv(2048)
     if data[2:4] != COMMAND_REQ:
         return -1, None
@@ -67,12 +90,13 @@ def command_server(conn, number, user):
     data = msg.decode('utf-8').split('\n')
 
     command = data[0]
-    param_1 = data[1]
+    params = data[1:]
 
-    message = command_res(command, msg).encode("utf-8")
+    message, current_dir = command_res(command, params, msg, user, current_dir)
+
+    message = message.encode("utf-8")
 
     data = encrypt(message, COMMAND_RES, 'server', str(number))
     conn.sendall(data)
 
-
-    return 0, None
+    return 0, current_dir
